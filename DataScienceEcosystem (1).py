768cc0c0bfd66959588f4b7549d0c2471894ec2c
joaotnunes <158074057+joaotnunes@users.noreply.github.com>
2024-08-02 16:25:14 +0100

    Add files via upload

---
+++ DataScienceEcosystem (1).py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# -  List popular languages for Data Science
+# -  Arithmetic expressions in Python
+# -  List of commonly used libraries used by Data Scientists
+# -  Data Science Tools
+
+# Some of the popular languages that Data Scientists use are:
+
+# 1. Python
+# 2. R
+# 3. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. NumPy
+# 2. Pandas
+# 3. Matplotlib
+
+# | Data Science Tools       |
+# |--------------------------|
+# | Jupyter Notebook         |
+# | RStudio                  |
+# | Apache Zeppelin          |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[4]:
+
+
+(3*4)+5
+# This a simple arithmetic expression to mutiply then add integers
+
+
+# In[6]:
+
+
+200/60
+# This will convert 200 minutes to hours by diving by 60
+
+
+# ## Author
+# João Nunes
+
+# In[ ]:
+
+
+
+
